c4bb5e44ed6a6b79bee7381b4b941e0e45e02110
Sebastiano Vigna <sebastiano.vigna@gmail.com>
2023-11-01 12:45:35 +0100

    Setters

--- src/lib.rs
+++ src/lib.rs
@@ -41,7 +41,7 @@ use dsi_progress_logger::*;
 
 stderrlog::new().init().unwrap();
 let mut pl = ProgressLogger::default();
-pl.item_name = "pumpkin";
+pl.item_name("pumpkin");
 pl.start("Smashing pumpkins...");
 for _ in 0..100 {
    // do something on each pumlkin
@@ -55,7 +55,7 @@ use dsi_progress_logger::*;
 
 stderrlog::new().init().unwrap();
 let mut pl = ProgressLogger::default();
-pl.item_name = "pumpkin";
+pl.item_name("pumpkin");
 pl.start("Smashing pumpkins...");
 for _ in 0..100 {
    // do something on each pumlkin
@@ -67,7 +67,8 @@ This progress logger will display information about  memory usage:
 use dsi_progress_logger::*;
 
 stderrlog::new().init().unwrap();
-let mut pl = ProgressLogger::default().display_memory();
+let mut pl = ProgressLogger::default();
+pl.display_memory(true);
 ```
 */
 use log::info;
@@ -80,20 +81,130 @@ use sysinfo::{Pid, ProcessExt, RefreshKind, System, SystemExt};
 mod utils;
 use utils::*;
 
+/**!
+
+Logging trait.
+
+Implemented by [`ProgressLog`] and by `Option<ProgressLog>`. This approach makes it possible to
+pass as a [`ProgressLog`] either a [`ProgressLogger`], an `Option<ProgressLogger>`, or even
+`Option::<ProgressLogger>::None`.
+
+*/
 pub trait ProgressLog {
+    /// Display memory information.
+    fn display_memory(&mut self, display_memory: bool) -> &mut Self;
+
+    /// Set the name of an item.
+    fn item_name(&mut self, item_name: impl AsRef<str>) -> &mut Self;
+
+    /// Set the log interval.
+    fn log_interval(&mut self, log_interval: Duration) -> &mut Self;
+
+    /// Set the expected number of updates.
+    ///
+    /// If not [`None`],
+    /// the logger will display the percentage of completion and
+    /// an estimate of the time to completion.
+    fn expected_updates(&mut self, expected_updates: Option<usize>) -> &mut Self;
+
+    /// Set the time unit to use for speed.
+    ///
+    /// If not [`None], the logger will always display the speed in this unit
+    /// instead of making a choice of readable unit based on the elapsed time. Moreover, large numbers
+    /// will not be thousands separated. This is useful when the output of the logger must be parsed.
+    fn time_unit(&mut self, time_unit: Option<TimeUnit>) -> &mut Self;
+
+    /// Set whether to display additionally the speed achieved during the last log interval.
+    fn local_speed(&mut self, local_speed: bool) -> &mut Self;
+
+    /// Start the logger, displaying the given message.
+    ///
+    /// You can pass the empty string to display nothing.
     fn start(&mut self, msg: impl AsRef<str>);
+
+    /// Increase the count and check whether it is time to log.
     fn update(&mut self);
+
+    /// Set the count and check whether it is time to log.
     fn update_with_count(&mut self, count: usize);
+
+    /// Increase the count but check whether it is time log only after an
+    /// implementation-defined number of calls.
+    ///
+    /// Useful for very short activities with respect to which  checking the time is expensive.
     fn light_update(&mut self);
+
+    /// Increase the count and force a log.
     fn update_and_display(&mut self);
+
+    /// Stop the logger, fixing the final time.
     fn stop(&mut self);
+
+    /// Stop the logger, print `Completed.`, and display the final stats.
+    /// The number of expected updates will be cleared.
     fn done(&mut self);
+
+    /// Stop the logger, set the count, print `Completed.`, and display the final stats.
+    /// The number of expected updates will be cleared.
+    ///
+    /// This method is particularly useful in two circumstances:
+    /// * you have updated the logger with some approximate values (e.g., in a multicore computation) but before
+    ///   printing the final stats you want the internal counter to contain an exact value;
+    /// * you have used the logger as a handy timer, calling just [`start`](#fields.start) and this method.
     fn done_with_count(&mut self, count: usize);
+
+    /// Return the elapsed time since the logger was started, or `None` if the logger has not been started.
     fn elapsed(&self) -> Option<Duration>;
+
+    /// Refresh memory information, if previously requested with [`display_memory`](#method.display_memory).
+    /// You do not need to call this method unless you display the logger manually.
     fn refresh(&mut self);
 }
 
 impl<P: ProgressLog> ProgressLog for Option<P> {
+    fn display_memory(&mut self, display_memory: bool) -> &mut Self {
+        if let Some(pl) = self {
+            pl.display_memory(display_memory);
+        }
+        self
+    }
+
+    fn item_name(&mut self, item_name: impl AsRef<str>) -> &mut Self {
+        if let Some(pl) = self {
+            pl.item_name(item_name);
+        }
+        self
+    }
+
+    fn log_interval(&mut self, log_interval: Duration) -> &mut Self {
+        if let Some(pl) = self {
+            pl.log_interval(log_interval);
+        }
+        self
+    }
+
+    fn expected_updates(&mut self, expected_updates: Option<usize>) -> &mut Self {
+        if let Some(pl) = self {
+            pl.expected_updates(expected_updates);
+        }
+        self
+    }
+
+    fn time_unit(&mut self, time_unit: Option<TimeUnit>) -> &mut Self {
+        if let Some(pl) = self {
+            pl.time_unit(time_unit);
+        }
+        self
+    }
+
+    /// Set whether to display additionally the speed achieved during the last log interval.
+    fn local_speed(&mut self, local_speed: bool) -> &mut Self {
+        if let Some(pl) = self {
+            pl.local_speed(local_speed);
+        }
+        self
+    }
+
     fn start(&mut self, msg: impl AsRef<str>) {
         if let Some(pl) = self {
             pl.start(msg);
@@ -153,20 +264,26 @@ impl<P: ProgressLog> ProgressLog for Option<P> {
     }
 }
 
-pub struct ProgressLogger<'a> {
+/**
+
+An implementation of [`ProgressLog`] with output generated using the [`log`](https://docs.rs/log) crate
+at the `info` level.
+
+*/
+pub struct ProgressLogger {
     /// The name of an item. Defaults to `item`.
-    pub item_name: &'a str,
+    item_name: String,
     /// The log interval. Defaults to 10 seconds.
-    pub log_interval: Duration,
+    log_interval: Duration,
     /// The expected number of updates. If set, the logger will display the percentage of completion and
     /// an estimate of the time to completion.
-    pub expected_updates: Option<usize>,
+    expected_updates: Option<usize>,
     /// The time unit to use for speed. If set, the logger will always display the speed in this unit
     /// instead of making a choice of readable unit based on the elapsed time. Moreover, large numbers
     /// will not be thousands separated. This is useful when the output of the logger must be parsed.
-    pub time_unit: Option<TimeUnit>,
+    time_unit: Option<TimeUnit>,
     /// Display additionally the speed achieved during the last log interval.
-    pub local_speed: bool,
+    local_speed: bool,
     start_time: Option<Instant>,
     last_log_time: Instant,
     next_log_time: Instant,
@@ -179,10 +296,10 @@ pub struct ProgressLogger<'a> {
     pid: Pid,
 }
 
-impl<'a> Clone for ProgressLogger<'a> {
+impl Clone for ProgressLogger {
     fn clone(&self) -> Self {
         Self {
-            item_name: self.item_name,
+            item_name: self.item_name.clone(),
             log_interval: self.log_interval,
             expected_updates: self.expected_updates,
             time_unit: self.time_unit,
@@ -202,10 +319,10 @@ impl<'a> Clone for ProgressLogger<'a> {
     }
 }
 
-impl<'a> Default for ProgressLogger<'a> {
+impl Default for ProgressLogger {
     fn default() -> Self {
         Self {
-            item_name: "item",
+            item_name: "item".into(),
             log_interval: Duration::from_secs(10),
             expected_updates: None,
             time_unit: None,
@@ -222,19 +339,11 @@ impl<'a> Default for ProgressLogger<'a> {
     }
 }
 
-impl<'a> ProgressLogger<'a> {
+impl ProgressLogger {
     /// Calls to [light_update](#method.light_update) will cause a call to
     /// [`Instant::now`] only if the current count
     /// is a multiple of this mask plus one.
     pub const LIGHT_UPDATE_MASK: usize = (1 << 20) - 1;
-
-    /// Chainable setter enabling memory display.
-    pub fn display_memory(mut self) -> Self {
-        if self.system.is_none() {
-            self.system = Some(System::new_with_specifics(RefreshKind::new().with_memory()));
-        }
-        self
-    }
 
     fn log(&mut self, now: Instant) {
         self.refresh();
@@ -265,7 +374,7 @@ impl<'a> ProgressLogger<'a> {
         f.write_fmt(format_args!(
             "{:.2} {}/{}, {:.2} {}/{}",
             items_per_second * time_unit_speed.as_seconds(),
-            pluralize(self.item_name, 2, false),
+            pluralize(&self.item_name, 2, false),
             time_unit_speed.label(),
             seconds_per_item / time_unit_timing.as_seconds(),
             time_unit_timing.label(),
@@ -276,9 +385,49 @@ impl<'a> ProgressLogger<'a> {
     }
 }
 
-impl<'a> ProgressLog for ProgressLogger<'a> {
-    /// Start the logger, displaying the given message.
-    /// You can pass the empty string to display nothing.
+impl ProgressLog for ProgressLogger {
+    /// Chainable setter enabling memory display.
+    fn display_memory(&mut self, display_memory: bool) -> &mut Self {
+        match (display_memory, &self.system) {
+            (true, None) => {
+                self.system = Some(System::new_with_specifics(RefreshKind::new().with_memory()));
+            }
+            (false, Some(_)) => {
+                self.system = None;
+            }
+            _ => (),
+        }
+        if self.system.is_none() {
+            self.system = Some(System::new_with_specifics(RefreshKind::new().with_memory()));
+        }
+        self
+    }
+
+    fn item_name(&mut self, item_name: impl AsRef<str>) -> &mut Self {
+        self.item_name = item_name.as_ref().into();
+        self
+    }
+
+    fn log_interval(&mut self, log_interval: Duration) -> &mut Self {
+        self.log_interval = log_interval;
+        self
+    }
+
+    fn expected_updates(&mut self, expected_updates: Option<usize>) -> &mut Self {
+        self.expected_updates = expected_updates;
+        self
+    }
+
+    fn time_unit(&mut self, time_unit: Option<TimeUnit>) -> &mut Self {
+        self.time_unit = time_unit;
+        self
+    }
+
+    fn local_speed(&mut self, local_speed: bool) -> &mut Self {
+        self.local_speed = local_speed;
+        self
+    }
+
     fn start(&mut self, msg: impl AsRef<str>) {
         let now = Instant::now();
         self.start_time = Some(now);
@@ -291,8 +440,7 @@ impl<'a> ProgressLog for ProgressLogger<'a> {
             info!("{}", msg.as_ref());
         }
     }
-    /// Refresh memory information, if previously requested with [`display_memory`](#methods.display_memory).
-    /// You do not need to call this method unless you display the logger manually.
+
     fn refresh(&mut self) {
         if let Some(system) = &mut self.system {
             system.refresh_memory();
@@ -300,13 +448,11 @@ impl<'a> ProgressLog for ProgressLogger<'a> {
         }
     }
 
-    /// Increase the count and check whether it is time to log.
     fn update(&mut self) {
         self.count += 1;
         self.log_if();
     }
 
-    /// Set the count and check whether it is time to log.
     fn update_with_count(&mut self, count: usize) {
         self.count += count;
         self.log_if();
@@ -321,19 +467,16 @@ impl<'a> ProgressLog for ProgressLogger<'a> {
         }
     }
 
-    /// Increase the count and force a log.
     fn update_and_display(&mut self) {
         self.count += 1;
         self.log(Instant::now());
     }
 
-    /// Stop the logger, fixing the final time.
     fn stop(&mut self) {
         self.stop_time = Some(Instant::now());
         self.expected_updates = None;
     }
 
-    /// Stop the logger, print `Completed.`, and display the final stats. The number of expected updates will be cleared.
     fn done(&mut self) {
         self.stop();
         info!("Completed.");
@@ -342,26 +485,17 @@ impl<'a> ProgressLog for ProgressLogger<'a> {
         info!("{}", self);
     }
 
-    /// Stop the logger, set the count, print `Completed.`, and display the final stats.
-    /// The number of expected updates will be cleared.
-    ///
-    /// This method is particularly useful in two circumstances:
-    /// * you have updated the logger with some approximate values (e.g., in a multicore computation) but before
-    ///   printing the final stats you want the internal counter to contain an exact value;
-    /// * you have used the logger as a handy timer, calling just [`start`](#fields.start) and this method.
-
     fn done_with_count(&mut self, count: usize) {
         self.count = count;
         self.done();
     }
 
-    /// Return the elapsed time since the logger was started, or `None` if the logger has not been started.
     fn elapsed(&self) -> Option<Duration> {
         self.start_time?.elapsed().into()
     }
 }
 
-impl<'a> Display for ProgressLogger<'a> {
+impl Display for ProgressLogger {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         if let Some(start_time) = self.start_time {
             let count_fmtd = if self.time_unit.is_none() {
@@ -383,7 +517,7 @@ impl<'a> Display for ProgressLogger<'a> {
                     f.write_fmt(format_args!(
                         " [{} {}, ",
                         count_fmtd,
-                        pluralize(self.item_name, self.count as isize, false)
+                        pluralize(&self.item_name, self.count as isize, false)
                     ))?;
                     self.fmt_timing_speed(f, seconds_per_item)?;
                     f.write_fmt(format_args!("]"))?
@@ -396,7 +530,7 @@ impl<'a> Display for ProgressLogger<'a> {
                 f.write_fmt(format_args!(
                     "{} {}, {}, ",
                     count_fmtd,
-                    pluralize(self.item_name, self.count as isize, false),
+                    pluralize(&self.item_name, self.count as isize, false),
                     TimeUnit::pretty_print(elapsed.as_millis()),
                 ))?;
 
